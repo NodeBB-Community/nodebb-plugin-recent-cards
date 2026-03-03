53227495f72110a084b749f64d264398561d9ac4
Barış Soner Uşaklı <barisusakli@gmail.com>
2026-03-03 17:22:37 -0500

    refactor: setup cards earlier
    don't wait for translator

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,38 +1,36 @@
 'use strict';
 
 $(document).ready(function () {
-	$(window).on('action:ajaxify.contentLoaded', async function () {
-		const recentCards = $('.recent-cards');
-		if (!recentCards.length) {
-			return;
-		}
-		const translator = await app.require('translator');
-		const [nextTitle, prevTitle] = await translator.translateKeys([
-			'[[global:pagination.nextpage]]', '[[global:pagination.previouspage]]',
-		], config.userLang);
-		if (!config.recentCards || !config.recentCards.enableCarousel) {
-			recentCards.removeClass('carousel-mode invisible');
-			return;
-		}
-		const rtl = $('html').attr('data-dir') === 'rtl';
-		const arrowClasses = 'link-secondary position-absolute top-50 translate-middle-y p-1 z-1';
-		const nextIcon = rtl ? 'fa-chevron-left' : 'fa-chevron-right';
-		const prevIcon = rtl ? 'fa-chevron-right' : 'fa-chevron-left';
-		const nextArrow = `<a href="#" class="${arrowClasses} ${rtl ? ' slick-prev' : ' slick-next'}" title="${nextTitle}">
+	const rtl = $('html').attr('data-dir') === 'rtl';
+	const arrowClasses = 'link-secondary position-absolute top-50 translate-middle-y p-1 z-1';
+	const nextIcon = rtl ? 'fa-chevron-left' : 'fa-chevron-right';
+	const prevIcon = rtl ? 'fa-chevron-right' : 'fa-chevron-left';
+	const nextArrow = `<a href="#" class="${arrowClasses} ${rtl ? ' slick-prev' : ' slick-next'}" title="">
 			<i class="fa-solid fa-fw ${nextIcon} fa-lg"></i>
 		</a>`;
-		const prevArrow = `<a href="#" class="${arrowClasses} ${rtl ? 'slick-next' : 'slick-prev'}" title="${prevTitle}">
+	const prevArrow = `<a href="#" class="${arrowClasses} ${rtl ? 'slick-next' : 'slick-prev'}" title="">
 			<i class="fa-solid fa-fw ${prevIcon} fa-lg"></i>
 		</a>`;
+
+	async function initSlick(container) {
+		if (!container.length || container.hasClass('slick-initialized')) {
+			return;
+		}
+
+		if (!config.recentCards || !config.recentCards.enableCarousel) {
+			container.removeClass('carousel-mode invisible');
+			return;
+		}
+
 		const slideCount = parseInt(config.recentCards.maxSlides, 10) || 4;
 		const slideMargin = 16;
 		const env = utils.findBootstrapEnvironment();
 		if (['xxl', 'xl', 'lg'].includes(env)) {
-			$('.recent-card').css({
-				width: (recentCards.width() - ((slideCount - 1) * slideMargin)) / slideCount,
+			container.find('.recent-card').css({
+				width: (container.width() - ((slideCount - 1) * slideMargin)) / slideCount,
 			});
 		}
-		recentCards.slick({
+		container.slick({
 			infinite: false,
 			slidesToShow: slideCount,
 			slidesToScroll: slideCount,
@@ -57,6 +55,27 @@ $(document).ready(function () {
 				},
 			}],
 		});
-		recentCards.removeClass('overflow-hidden invisible');
+
+		container.removeClass('overflow-hidden invisible');
+
+		container.find(rtl ? '.slick-prev' : '.slick-next').translateAttr(
+			'title', rtl ?
+				'[[global:pagination.previouspage]]' :
+				'[[global:pagination.nextpage]]');
+		container.find(rtl ? '.slick-next' : '.slick-prev').translateAttr(
+			'title', rtl ?
+				'[[global:pagination.nextpage]]' :
+				'[[global:pagination.previouspage]]');
+	}
+
+	$('.recent-cards').each(function () {
+		const recentCards = $(this);
+		initSlick(recentCards);
+	});
+
+	$(window).on('action:ajaxify.contentLoaded', function () {
+		$('.recent-cards').each((index, element) => {
+			initSlick($(element));
+		});
 	});
 });
